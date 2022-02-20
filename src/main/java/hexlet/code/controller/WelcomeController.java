d97ae594867cf0c5efe5b9a4ee75fdf5ac26f520
silentlyexisting <silentlyexistin@gmail.com>
2022-02-20 16:25:44 +0200

    initialization and deploy

---
+++ src/main/java/hexlet/code/controller/WelcomeController.java
@@ -0,0 +1,16 @@
+package hexlet.code.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/welcome")
+public class WelcomeController {
+
+    @GetMapping
+    public String getRootPage() {
+        return "Welcome to Spring";
+    }
+
+}
