fb1058751d2b3d5b66484c65c991e386495e116b
silentlyexisting <silentlyexistin@gmail.com>
2022-02-21 19:47:28 +0200

    added liquibase

---
+++ src/main/java/hexlet/code/model/User.java
@@ -0,0 +1,28 @@
+package hexlet.code.model;
+
+import org.hibernate.annotations.CreationTimestamp;
+
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+import javax.persistence.Id;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import java.util.Date;
+
+
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String password;
+
+    @CreationTimestamp
+    private Date createdAt;
+}
