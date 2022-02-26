2496e82120ebeaf47bc63c7d58f8928566dd063e
silentlyexisting <silentlyexistin@gmail.com>
2022-02-26 17:22:23 +0200

    added exception handler

--- src/main/java/hexlet/code/exception/GlobalExceptionHandler.java
+++ src/main/java/hexlet/code/exception/GlobalExceptionHandler.java
@@ -1,8 +1,33 @@
 package hexlet.code.exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.nio.file.AccessDeniedException;
+
+import static org.springframework.http.HttpStatus.*;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
+    @ResponseStatus(INTERNAL_SERVER_ERROR)
+    @ExceptionHandler(Exception.class)
+    public String internalServerErrorExceptionHandler(Exception exception) {
+        return exception.getMessage();
+    }
+
+    @ResponseStatus(UNAUTHORIZED)
+    @ExceptionHandler(UsernameNotFoundException.class)
+    public String userNotExceptionHandler(UsernameNotFoundException exception) {
+        return exception.getMessage();
+    }
+
+    @ResponseStatus(FORBIDDEN)
+    @ExceptionHandler(AccessDeniedException.class)
+    public String accessDeniedExceptionHandler(AccessDeniedException exception) {
+        return exception.getMessage();
+    }
 }
