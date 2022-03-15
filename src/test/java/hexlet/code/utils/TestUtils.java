7587078386e857cf4659a7200688f48a68974f0f
silentlyexisting <silentlyexistin@gmail.com>
2022-03-15 19:05:25 +0200

    fixed utils and added task status handler

--- src/test/java/hexlet/code/utils/TestUtils.java
+++ src/test/java/hexlet/code/utils/TestUtils.java
@@ -2,8 +2,13 @@ package hexlet.code.utils;
 
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.github.database.rider.core.api.dataset.DataSet;
+import com.github.database.rider.junit5.api.DBRider;
 import hexlet.code.config.security.component.TokenGenerator;
 import hexlet.code.dto.UserDto;
+import hexlet.code.model.User;
+import hexlet.code.repository.TaskStatusRepository;
+import hexlet.code.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mock.web.MockHttpServletResponse;
 import org.springframework.stereotype.Component;
@@ -15,12 +20,15 @@ import static org.springframework.http.MediaType.APPLICATION_JSON;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.http.HttpHeaders.AUTHORIZATION;
 import static hexlet.code.controller.UserController.USERS_CONTROLLER_PATH;
+import static hexlet.code.controller.TaskStatusController.TASK_STATUS_CONTROLLER_PATH;
 
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.Map;
 
+@DBRider
+@DataSet("dataset.yml")
 @Component
 public class TestUtils {
 
@@ -29,12 +37,19 @@ public class TestUtils {
     public static final String BASE_URL = "/api";
     public static final String DEFAULT_USER_DATA = "defaultUserData.json";
     public static final String UPDATE_USER_DATA = "updateUserData.json";
+    public static final String DEFAULT_TASK_STATUS = "defaultTaskStatus.json";
 
     @Autowired
     private MockMvc mockMvc;
 
     @Autowired
     private TokenGenerator tokenGenerator;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private TaskStatusRepository taskStatusRepository;
 
     public String readFileContent(String path) throws IOException {
         return Files.readString(Path.of(path).toAbsolutePath().normalize());
@@ -52,6 +67,19 @@ public class TestUtils {
                         .content(json)
                         .contentType(APPLICATION_JSON)
         ).andReturn().getResponse();
+    }
+
+    public void regDefaultTaskStatus() throws Exception {
+        String json = readFileContent(FIXTURES_PATH + DEFAULT_TASK_STATUS);
+        final User user = userRepository.findAll().get(0);
+        MockHttpServletResponse response =  perform(
+                post(BASE_URL + TASK_STATUS_CONTROLLER_PATH)
+                        .contentType(APPLICATION_JSON)
+                        .content(json),
+                user.getEmail()
+        ).andReturn().getResponse();
+
+//        return taskStatusRepository.findAll().get(0);
     }
 
 
