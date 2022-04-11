99d25c8b43fc823e331ab184d999bc3104043e57
silentlyexisting <silentlyexistin@gmail.com>
2022-04-11 21:03:50 +0300

    fixed env rollbar

--- src/main/java/hexlet/code/config/rollbar/RollbarConfig.java
+++ src/main/java/hexlet/code/config/rollbar/RollbarConfig.java
@@ -3,13 +3,13 @@ package hexlet.code.config.rollbar;
 import com.rollbar.notifier.Rollbar;
 import com.rollbar.notifier.config.Config;
 import com.rollbar.spring.webmvc.RollbarSpringConfigBuilder;
-import lombok.extern.slf4j.Slf4j;
+//import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
-@Slf4j
+//@Slf4j
 @Configuration
 @ComponentScan({
 
@@ -21,23 +21,21 @@ public class RollbarConfig {
     @Value("${rollbar_token:}")
     private String rollbarToken;
 
-    @Value("${spring.profiles.active:}")
-    private String activeProfile;
+//    @Value("${spring.profiles.active:}")
+//    private String activeProfile;
 
     /**
      * Register a Rollbar bean to configure App with Rollbar.
      */
     @Bean
     public Rollbar rollbar() {
-        log.info("rollbar token {}", rollbarToken);
         return new Rollbar(getRollbarConfigs(rollbarToken));
     }
 
     private Config getRollbarConfigs(String accessToken) {
 
         return RollbarSpringConfigBuilder.withAccessToken(accessToken)
-                .environment("prod")
-                .enabled(activeProfile == "prod")
+                .environment("development")
                 .build();
     }
 }
