8ff62b51316ca72962156ed647e732d97f753275
silentlyexisting <silentlyexistin@gmail.com>
2022-04-11 16:22:16 +0300

    added rollbar

---
+++ src/main/java/hexlet/code/config/rollbar/RollbarConfig.java
@@ -0,0 +1,34 @@
+package hexlet.code.config.rollbar;
+
+import com.rollbar.notifier.Rollbar;
+import com.rollbar.notifier.config.Config;
+import com.rollbar.spring.webmvc.RollbarSpringConfigBuilder;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan({
+        "hexlet.code"
+})
+public class RollbarConfig {
+    @Value("${rollbar_token:}")
+    private String rollbarToken;
+
+    @Value("${spring.profiles.active:}")
+    private String activeProfile;
+
+    @Bean
+    public Rollbar rollbar() {
+        return new Rollbar(getRollbarConfigs(rollbarToken));
+    }
+
+    private Config getRollbarConfigs(String accessToken) {
+
+        return RollbarSpringConfigBuilder.withAccessToken(accessToken)
+                .environment("development")
+                .enabled(activeProfile == "prod")
+                .build();
+    }
+}
